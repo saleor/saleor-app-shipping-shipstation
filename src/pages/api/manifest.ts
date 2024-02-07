1f5b7a1474c2722dec13abae192da2fb7dea8627
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-02-07 10:13:42 +0100

    webhook on order

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -2,20 +2,11 @@ import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { AppManifest } from "@saleor/app-sdk/types";
 
 import packageJson from "../../../package.json";
-import { orderCreatedWebhook } from "./webhooks/order-created";
+import { orderFilterShippingMethodsWebhook } from "./webhooks/order-filter-shipping-methods";
 import { shippingListMethodsForCheckoutWebhook } from "./webhooks/shipping-list-methods-for-checkout";
 
-/**
- * App SDK helps with the valid Saleor App Manifest creation. Read more:
- * https://github.com/saleor/saleor-app-sdk/blob/main/docs/api-handlers.md#manifest-handler-factory
- */
 export default createManifestHandler({
   async manifestFactory({ appBaseUrl, request }) {
-    /**
-     * Allow to overwrite default app base url, to enable Docker support.
-     *
-     * See docs: https://docs.saleor.io/docs/3.x/developer/extending/apps/local-app-development
-     */
     const iframeBaseUrl = process.env.APP_IFRAME_BASE_URL ?? appBaseUrl;
     const apiBaseURL = process.env.APP_API_BASE_URL ?? appBaseUrl;
 
@@ -23,39 +14,14 @@ export default createManifestHandler({
       name: "Saleor App Shipping",
       tokenTargetUrl: `${apiBaseURL}/api/register`,
       appUrl: iframeBaseUrl,
-      /**
-       * Set permissions for app if needed
-       * https://docs.saleor.io/docs/3.x/developer/permissions
-       */
-      permissions: [
-        /**
-         * Add permission to allow "ORDER_CREATED" webhook registration.
-         *
-         * This can be removed
-         */
-        "MANAGE_ORDERS",
-        "MANAGE_CHECKOUTS",
-        "IMPERSONATE_USER",
-        "MANAGE_SHIPPING",
-      ],
+
+      permissions: ["MANAGE_ORDERS", "MANAGE_CHECKOUTS", "IMPERSONATE_USER", "MANAGE_SHIPPING"],
       id: "saleor.app",
       version: packageJson.version,
-      /**
-       * Configure webhooks here. They will be created in Saleor during installation
-       * Read more
-       * https://docs.saleor.io/docs/3.x/developer/api-reference/webhooks/objects/webhook
-       *
-       * Easiest way to create webhook is to use app-sdk
-       * https://github.com/saleor/saleor-app-sdk/blob/main/docs/saleor-webhook.md
-       */
       webhooks: [
-        orderCreatedWebhook.getWebhookManifest(apiBaseURL),
         shippingListMethodsForCheckoutWebhook.getWebhookManifest(apiBaseURL),
+        orderFilterShippingMethodsWebhook.getWebhookManifest(apiBaseURL),
       ],
-      /**
-       * Optionally, extend Dashboard with custom UIs
-       * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
-       */
       extensions: [],
       author: "Saleor Commerce",
       brand: {
